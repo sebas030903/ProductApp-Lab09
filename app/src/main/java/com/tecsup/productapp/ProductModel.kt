fc7da44e0f87ec761630cc7ef69aac0650f5a25c
sebas030903 <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-05-19 23:39:51 -0500

    Creamos la interfaz

---
+++ app/src/main/java/com/tecsup/productapp/ProductModel.kt
@@ -0,0 +1,18 @@
+package com.tecsup.productapp
+
+data class ProductResponse(
+    val products: List<Product>
+)
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Double,
+    val discountPercentage: Double,
+    val rating: Double,
+    val stock: Int,
+    val brand: String,
+    val category: String,
+    val thumbnail: String
+)
